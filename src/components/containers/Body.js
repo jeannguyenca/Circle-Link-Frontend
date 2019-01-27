30084b2a4067e8e18eb4ddfa17ba59c3ca1d5c26
Jean <jeannguyen.ca@gmail.com>
2019-01-26 16:49:45 -0800

    Add file structures

---
+++ src/components/containers/Body.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+//user ROUTE to switch from different pages
+class Body extends Component {
+  render() {
+    return <div className="container">This is body</div>;
+  }
+}
+export default Body;
