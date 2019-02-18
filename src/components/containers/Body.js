6e480bb91808b9c59f59bede33891aa8b0136129
Jean <jeannguyen.ca@gmail.com>
2019-02-18 11:34:36 -0800

    Test user login

--- src/components/containers/Body.js
+++ src/components/containers/Body.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import Login from "../pages/Login"
 
 //user ROUTE to switch from different pages
 class Body extends Component {
   render() {
-    return <div className="container">This is body</div>;
+    return <div className="container">
+      <Login />
+    </div>;
   }
 }
 export default Body;
